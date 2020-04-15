04c5899350f50302906e5fcdd0dc06bb26c36d00
Lingareddyvinitha <r151453@rguktrkv.ac.in>
2020-04-15 14:33:19 +0000

    added grid-game ..

--- src/components/GridGame/Cell/styledCompoenent.js
+++ src/components/GridGame/Cell/styledCompoenent.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import tw from 'tailwind.macro';
 
-const Grid = styled.div `${tw `bg-gray-600 w-24	 h-24 m-2`}`
+const Grid = styled.div `${tw `bg-gray-600 w-24	 h-24 m-1`}`
 
 
 export { Grid }
